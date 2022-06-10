d9da583ec41e20ee9a3add43e51a80c260d39898
Srihari Radhakrishna <srihari@Sriharis-MacBook-Pro.local>
2022-06-10 17:57:50 +0530

    service updates

--- mnist-classification-keras/service/main.py
+++ mnist-classification-keras/service/main.py
@@ -1,23 +1,26 @@
-import logging
 import os
-
+import numpy as np
+import mlfoundry as mlf
+from PIL import Image
+from io import BytesIO
+from fastapi import File, UploadFile
+from skimage.transform import resize
 from servicefoundry.service import fastapi
 
-logger = logging.getLogger(__name__)
-
 app = fastapi.app()
+mlf_client = mlf.get_client(api_key=os.environ.get('TFY_API_KEY'))
+run = mlf_client.get_run(os.environ.get('TFY_RUN_ID'))
+model = run.get_model()
 
+def recognize_digit(image):
+    image = image.reshape(1, 28, 28, 1)  # add a batch dimension
+    prediction = model.predict(image).tolist()[0]
+    return {str(i): prediction[i] for i in range(10)}
 
-@app.get(path="/add")
-def add(a: int, b: int):
-    return a + b
+def load_image_into_numpy_array(data):
+    return np.array(Image.open(BytesIO(data)))
 
-
-@app.get(path="/subtract")
-def subtract(a: int, b: int):
-    return a - b
-
-
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+@app.post('/predict')
+async def predict(image: UploadFile = File(...)):
+    np_image = load_image_into_numpy_array(await image.read())
+    return {'predictions': recognize_digit(resize(np_image, (28, 28, 1)))}
