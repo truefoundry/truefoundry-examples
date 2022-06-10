2819cbba912ea8e6c04c1fb96bed163976d5e181
Srihari Radhakrishna <srihari@Sriharis-MacBook-Pro.local>
2022-06-10 15:23:09 +0530

    add webapp and service separately

--- mnist-classification-keras/service/main.py
+++ mnist-classification-keras/service/main.py
@@ -1,21 +1,23 @@
+import logging
 import os
 
-import gradio as gr
-import mlfoundry as mlf
+from servicefoundry.service import fastapi
 
-MLF_API_KEY = os.environ.get('TFY_API_KEY')
-MLF_RUN_ID = os.environ.get('TFY_RUN_ID')
+logger = logging.getLogger(__name__)
 
-mlf_api = mlf.get_client(api_key=MLF_API_KEY)
-run = mlf_api.get_run(MLF_RUN_ID)
-model = run.get_model()
+app = fastapi.app()
 
-def recognize_digit(image):
-    image = image.reshape(1, 28, 28, 1)  # add a batch dimension
-    prediction = model.predict(image).tolist()[0]
-    return {str(i): prediction[i] for i in range(10)}
 
-gr.Interface(fn=recognize_digit,
-             inputs="sketchpad",
-             outputs=gr.outputs.Label(num_top_classes=3),
-             title="MNIST Sketchpad").launch(server_name="0.0.0.0");
+@app.get(path="/add")
+def add(a: int, b: int):
+    return a + b
+
+
+@app.get(path="/subtract")
+def subtract(a: int, b: int):
+    return a - b
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
