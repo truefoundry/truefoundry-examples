5fe16ae75cba7f8cd19b6828c0fd1c838b453290
debajyoti-truefoundry <debajyoti@truefoundry.com>
2022-09-19 15:58:00 +0530

    chore: update `register_class` API call

--- deployment/function/hf_inference_function_deployment/deploy.py
+++ deployment/function/hf_inference_function_deployment/deploy.py
@@ -1,6 +1,6 @@
 import logging
 
-from servicefoundry.function_service import FunctionService, remote
+from servicefoundry.function_service import FunctionService
 from servicefoundry import Resources
 
 from inference import Model
@@ -12,7 +12,6 @@ service = FunctionService(
     resources=Resources(memory_request=1000, memory_limit=1500),
 )
 
-deployble_model_class = remote(Model, init_kwargs={"model_fqn": "t5-small"})
-service.register_class(deployble_model_class)
+service.register_class(Model, init_kwargs={"model_fqn": "t5-small"})
 
 service.deploy(workspace_fqn="v1:local:my-ws-2")
