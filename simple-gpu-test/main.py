fe00e7d02a13c434f7915958b0a7776672e364e1
srihari radhakrishna <100119331+srihari-tf@users.noreply.github.com>
2023-04-21 17:37:19 +0530

    Update main.py

--- simple-gpu-test/main.py
+++ simple-gpu-test/main.py
@@ -1,6 +1,5 @@
 import torch
 if torch.cuda.is_available():
-    for idx in torch.cuda.device_count():
-        print(torch.cuda.get_device_name(idx))
+    print(str(torch.cuda.device_count()) + ' GPUs availabe')
 else:
     print('Unable to use GPUs with torch!')
