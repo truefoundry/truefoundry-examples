11ff0ef23e07cbe1f51408cd967b683dfded13b4
Nikhil Popli <97437109+nikp1172@users.noreply.github.com>
2023-04-26 20:06:15 +0530

    Update train.py

--- end-to-end-examples/diabetes-regression/train/train.py
+++ end-to-end-examples/diabetes-regression/train/train.py
@@ -43,8 +43,9 @@ def train(kernel: str, n_quantiles: int):
     # create a run, setting the project's name the following run
     # should be associated with via setting the `ml_repo`
     # and the name of the run name via `run_name`
+    mlfoundry.get_client()
     ml_repo = client.create_ml_repo(os.environ['ML_REPO_NAME'])
-    run = mlfoundry.get_client().create_run(
+    run = client.create_run(
         ml_repo=ml_repo.name, run_name="SVR-with-QT"
     )
 
