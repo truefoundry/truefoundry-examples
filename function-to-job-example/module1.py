33ea38190ee503cab977840341f6dafd9fe8a1fb
Aditya Jha <adityajha@Adityas-MacBook-Air.local>
2023-08-10 13:20:39 +0530

    remove to_list() as not needed

--- function-to-job-example/module1.py
+++ function-to-job-example/module1.py
@@ -3,4 +3,4 @@ from typing_extensions import Annotated
 import numpy as np
 
 def normal(loc: Annotated[float, typer.Option()] = 1, scale: Annotated[float, typer.Option()] = 2):
-    return np.random.normal(loc=loc, scale=scale).tolist()
+    return np.random.normal(loc=loc, scale=scale)
--- function-to-job-example/module2.py
+++ function-to-job-example/module2.py
@@ -3,4 +3,4 @@ from typing_extensions import Annotated
 import numpy as np
 
 def uniform(low: Annotated[float, typer.Option()] = 1, high: Annotated[float, typer.Option()] = 1):
-    return np.random.uniform(low=low, high=high).tolist()
+    return np.random.uniform(low=low, high=high)
